813474a52e481c27ba417d8968f784b53b1a01f4
priyxnshutiwari <artisanpriyanshu@gmail.com>
2026-06-01 23:04:15 +0530

    Made Authentication page

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,8 +1,19 @@
-import React from "react"
+export const meta = () => ([
+    { title: 'Resumind | Auth'},
+    { name: 'description', content: 'Log into your account'},
+])
 
 const Auth = () => {
     return (
-        <div>Auth</div>
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
+            <div className= "gradient-border shadow-lg"></div>
+               <section className= "flex flex-col gap-8 bg-white rounded-2xl p-10">
+                   <div className="flex flex-col items-center gap-2 text-center">
+                       <h1>Welcome</h1>
+                       <h2>Log In to Continue Your Job Journey</h2>
+                   </div>
+               </section>
+        </main>
     )
 }
 
